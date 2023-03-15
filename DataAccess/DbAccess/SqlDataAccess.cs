412062e5e4513254d2636904381ab17886204329
Yakubu Adeyemi Oseni <iamyakose@outlook.com>
2023-03-15 08:27:09 +0100

    first commit

---
+++ DataAccess/DbAccess/SqlDataAccess.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+using System.Data;
+using Dapper;
+
+namespace DataAccess.DbAccess
+{
+    public class SqlDataAccess : ISqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task<IEnumerable<T>> LoadData<T, U>(string storeProcedure,
+                                    U parameters, string connnectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connnectionId));
+
+            return await connection.QueryAsync<T>(storeProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+        public async Task SaveData<T>(string storeProcedure,
+                                    T parameters, string connnectionId = "Default")
+        {
+            using IDbConnection connection = new SqlConnection(_config.GetConnectionString(connnectionId));
+
+            await connection.ExecuteAsync(storeProcedure, parameters, commandType: CommandType.StoredProcedure);
+
+        }
+    }
+}
---
+++ UserDB/Script.PostDeployment.sql
@@ -0,0 +1,8 @@
+﻿if not exists (select 1 from dbo.[User])
+begin
+  insert into dbo.[User] (FirstName, LastName)
+  values ('Yemi', 'Oseni'),
+   ('Sue', 'Storm'),
+   ('Cassy', 'Larssy'),
+   ('Andrew', 'Tijani');
+end
